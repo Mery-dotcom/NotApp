69ab7d9b4844a1eba060cf2836f9a65619bf1aa5
Mery-dotcom <meka-28list.ru>
2024-12-08 17:55:13 +0600

    Add logic

---
+++ app/src/main/java/com/geeks/notapp/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geeks.notapp.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geeks.notapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
